dde7e42087b3ea2e46d16076afbbebb7f43d94ad
Mustafa Ahmed <mahme19@student.sdu.dk>
2021-03-24 12:14:36 +0100

    common and core implemented

---
+++ Game/Common/src/main/java/dk/sdu/common/services/IPostEntityProcessingService.java
@@ -0,0 +1,13 @@
+package dk.sdu.common.services;
+
+import dk.sdu.common.data.GameData;
+import dk.sdu.common.data.World;
+
+/**
+ *
+ * @author jcs
+ */
+public interface IPostEntityProcessingService {
+
+    void process(GameData gameData, World world);
+}
