17fa8b56a7d7b88cd0713cb4fe238f22eee90e2f
Yusaf Tareen <gayssuck123@gmail.com>
2021-05-25 15:15:37 +0200

    Unit Test - EnemyPluginTest
    - Test if enemy module is successfully created.

--- Game/Core/src/test/java/dk/sdu/enemysystem/EnemyPluginTest.java
+++ Game/Core/src/test/java/dk/sdu/enemysystem/EnemyPluginTest.java
@@ -1,41 +1,42 @@
-package dk.sdu.player;
+package dk.sdu.enemysystem;
 
 import dk.sdu.common.data.Entity;
 import dk.sdu.common.data.GameData;
 import dk.sdu.common.data.World;
-import dk.sdu.commonplayer.Player;
+import dk.sdu.common.enemy.Enemy;
 import junit.framework.TestCase;
 
 /**
  * @Author Yusaf
  */
 
-public class PlayerPluginTest extends TestCase {
-    private PlayerPlugin playerPlugin;
+public class EnemyPluginTest extends TestCase {
+    private EnemyPlugin enemyPlugin;
     private GameData gameData;
     private World world;
 
-    public PlayerPluginTest() {
+    public EnemyPluginTest() {
     }
 
     public void setUp() throws Exception {
-        this.playerPlugin = new PlayerPlugin();
+        this.enemyPlugin = new EnemyPlugin();
         this.gameData = new GameData();
         this.world = new World();
     }
 
     public void testStart() {
         boolean testResult = false;
-        playerPlugin.start(gameData, world);
+        enemyPlugin.start(gameData, world);
+        enemyPlugin.startWave(gameData, world, 1);
 
         for (Entity entity : world.getEntities()) {
-            if (entity instanceof Player){
+            if (entity instanceof Enemy) {
                 testResult = true;
                 break;
             }
         }
         assertTrue(testResult);
-        System.out.println("TEST - Player Created: " + testResult);
+        System.out.println("TEST - Enemy created: " + testResult);
     }
 
 }
